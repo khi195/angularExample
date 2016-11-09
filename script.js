ebbec2d864eddb49894495116cadcf08d699264e
Kashif Iqbal <Kashif.h.iqbal@gmail.com>
2016-11-09 18:39:29 +0000

    angular schema form logic

--- script.js
+++ script.js
@@ -1,19 +1,49 @@
 // Code goes here
-var app = angular. module('bravissimoExampleApp', []);
+var app = angular. module('bravissimoExampleApp', ['schemaForm']);
 
-app.controller('submitForm', function($scope){
+app.controller('submitForm', function($scope) {
+  $scope.schema = {
+  "type": "object",
+  "title": "Comment",
+  "properties": {
+    "name": {
+      "title": "Name",
+      "type": "string"
+    },
+    "email": {
+      "title": "Email",
+      "type": "string",
+      "pattern": "^\\S+@\\S+$",
+      "description": "Email will be used for evil."
+    },
+    "comment": {
+      "title": "Comment",
+      "type": "string",
+      "maxLength": 20,
+      "validationMessage": "Don't be greedy!"
+    }
+  },
+  "required": [
+    "name",
+    "email",
+    "comment"
+  ]
+};
 
-$scope.submit = function(isValid){
-console.log(this.submitUser);
-console.log(this.submitEmail);
-console.log(this.submitMessage);
-if(isValid){
-    alert('Good Job')
-}
-else{
-    alert('Form failed')
-}
+  $scope.form = [
+  "name",
+  "email",
+  {
+    "key": "comment",
+    "type": "textarea",
+    "placeholder": "Make a comment"
+  },
+  {
+    "type": "submit",
+    "style": "btn-info",
+    "title": "Submit"
+  }
+];
 
-}
-
+  $scope.model = {};
 });

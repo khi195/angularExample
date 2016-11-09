82b20def746efe09a5ec8d8be840a66835696d90
Kashif Iqbal <Kashif.h.iqbal@gmail.com>
2016-11-09 13:03:07 +0000

    Controller for accepting data from login form add

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 // Code goes here
+var app = angular. module('bravissimoExampleApp', []);
 
+app.controller('submitForm', function($scope){
+
+$scope.submit = function(){
+console.log(this.submitUser);
+console.log(this.submitEmail);
+console.log(this.submitMessage);
+}
+
+});

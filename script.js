90ebe9abd420207a68ca4d6bbed0a8e24e98b29c
Kashif Iqbal <Kashif.h.iqbal@gmail.com>
2016-11-09 16:01:56 +0000

    logic for form validation

--- script.js
+++ script.js
@@ -3,10 +3,17 @@ var app = angular. module('bravissimoExampleApp', []);
 
 app.controller('submitForm', function($scope){
 
-$scope.submit = function(){
+$scope.submit = function(isValid){
 console.log(this.submitUser);
 console.log(this.submitEmail);
 console.log(this.submitMessage);
+if(isValid){
+    alert('Good Job')
+}
+else{
+    alert('Form failed')
+}
+
 }
 
 });
